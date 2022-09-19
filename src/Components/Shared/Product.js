73b8a2e7bc093a0251a42365d971594125077097
saeedeh torkamani <saeedehtorkamani@gmail.com>
2022-09-19 02:28:47 -0700

    replace helper file to src

--- src/Components/Shared/Product.js
+++ src/Components/Shared/Product.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+//funtctions
+import { shorten } from '../../Helpers/functions';
+
 const Product = ({productData}) => {
     return (
         <div>
             <img src={productData.image} alt='product' style={{width:'200px'}} />
-            <h3>{productData.title}</h3>
+            <h3>{shorten(productData.title)}</h3>
             <p>{productData.price} </p>
             <div>
                 <a>Details</a>
